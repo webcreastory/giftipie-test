af69ce96d9d0c2ff31d8c0f2d45097152dd667bc
torong <hi_ryuji@naver.com>
2024-01-30 01:57:08 +0900

    Design: 초기 CSS 세팅

---
+++ src/styles/theme.jsx
@@ -0,0 +1,10 @@
+const theme = {
+  mainColor: "#6ABD8C",
+  mainColorLight: "#CCDFB0",
+  backgroundColor: "#F6F9F0",
+  darkGray: "#595959",
+  lightGray: "#939292",
+  superLightGray: "#F1F2F5",
+};
+
+export default theme;
